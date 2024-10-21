5da5eb79da09ef65331eab5e78cf568ccf9baf8f
AnshRana-coder <anshjatrana555@gmail.com>
2024-10-21 12:50:15 +0530

    q17

---
+++ Assignments/Assignment 1/q1.c
@@ -0,0 +1,12 @@
+//  WAP to check whether a given is Armstrong or not
+#include<stdio.h>
+int maini(){
+    int a;
+    printf("Enter a no");
+    scanf("%d",&a);
+    int count=0;
+
+    while(a%10!=0){
+         count++;
+    }
+}
--- Lab-Manual/q17.c
+++ Lab-Manual/q17.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 int main(){
     char a;
-    printf("Enter a char ");
+    printf("Enter a character");
     scanf("%c",&a);
-    if(a=='a'||a=='e' ||a=='i'||a=='0'||a=='u' ||a=='A' ||a=='E'||a=='I' ||a=='0'||a==")"  )
-    printf("%c is vowel",a);
-    
-     else
-     printf("conconent");
+    int x=a;
+    if(x>=65||x<=90 || x<=122||x>=97){
+        if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+            printf("ENterted aalphabet is Vowel");
+        }
+        else{
+            printf("its consonenst");
+        }
+
+    }
+    else{
+        printf("entered thung is not a alphabet");
+    }
     return 0;
 }
